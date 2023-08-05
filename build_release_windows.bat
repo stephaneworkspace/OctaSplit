6c72036b87a7ba06085c2f2ff09cce44b558f428
Stéphane Bressani <stephane@bressani.dev>
2023-08-05 23:08:13 +0200

    ajout du build automatisé

--- build_release_windows.bat
+++ build_release_windows.bat
@@ -21,4 +21,8 @@ REM cmake -DCMAKE_BUILD_TYPE=Release -A x64 ..
 
 REM Compiler le projet
 cmake --build . --target OctaSplit --config Release -- /m
+
+REM Compiler le projet avec MSBuild
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" OctaSplit.sln /p:Configuration=Release /p:Platform=x6
+
 echo Compilation terminé
