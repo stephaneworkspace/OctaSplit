a310cd839584ecb938ec2c1e78776e45d21d57e0
Stéphane Bressani <stephane@bressani.dev>
2023-08-05 23:28:52 +0200

    clean

--- clean_mswin.bat
+++ clean_mswin.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Suppression des répertoires...
-rd /s /q "_deps" "CMakeFiles" "OctaSplit.dir" "OctaSplit_artefacts" "x64" ".vs"
+rd /s /q "_deps" "CMakeFiles" "OctaSplit.dir" "OctaSplit_artefacts" "x64" ".vs" "build"
 
 echo Suppression des fichiers...
 del /q ALL_BUILD.vcxproj ALL_BUILD.vcxproj.filters ALL_BUILD.vcxproj.user cmake_install.cmake CMakeCache.txt CPackConfig.cmake CPackSourceConfig.cmake INSTALL.vcxproj INSTALL.vcxproj.filters OctaSplit.sln OctaSplit.vcxproj OctaSplit.vcxproj.filters PACKAGE.vcxproj PACKAGE.vcxproj.filters ZERO_CHECK.vcxproj ZERO_CHECK.vcxproj.filters
