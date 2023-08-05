67a30b651273243ff1c7a5385896487b683af6a9
Stéphane Bressani <stephane@bressani.dev>
2023-08-05 22:52:27 +0200

    script de compilation

--- build_release_windows.bat
+++ build_release_windows.bat
@@ -3,7 +3,7 @@ echo Suppression des répertoires...
 rd /s /q "_deps" "CMakeFiles" "OctaSplit.dir" "OctaSplit_artefacts" "x64"
 
 echo Suppression des fichiers...
-del /q ALL_BUILD.vcxproj ALL_BUILD.vcyproj.filters ALL_BUILD.vcxproj.user cmake_install.cmake CMakeCache.txt CPackConfig.cmake CPackSourceConfig.cmake INSTALL.vcxproj INSTALL.vcxproj.filters OctaSplit.sln OctaSplit.vcxproj OctaSplit.vcxproj.filters PACKAGE.vcxproj PACKAGE.vcxproj.filters ZERO_CHECK.vcxproj ZERO_CHECK.vcxproj.filters
+del /q ALL_BUILD.vcxproj ALL_BUILD.vcxproj.filters ALL_BUILD.vcxproj.user cmake_install.cmake CMakeCache.txt CPackConfig.cmake CPackSourceConfig.cmake INSTALL.vcxproj INSTALL.vcxproj.filters OctaSplit.sln OctaSplit.vcxproj OctaSplit.vcxproj.filters PACKAGE.vcxproj PACKAGE.vcxproj.filters ZERO_CHECK.vcxproj ZERO_CHECK.vcxproj.filters
 
 echo Le nettoyage est terminé.
 
@@ -20,5 +20,5 @@ REM vous pouvez spécifier les architectures cibles comme ceci si nécessaire
 REM cmake -DCMAKE_BUILD_TYPE=Release -A x64 ..
 
 REM Compiler le projet
-cmake --build . --config Release
+cmake --build . --config Release -- /m /project OctaSplit
 echo Compilation terminé
